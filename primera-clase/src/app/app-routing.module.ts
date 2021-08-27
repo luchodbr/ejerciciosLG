9232638b1f3986c03d5e7ae8bd33c8afb926c713
Luciano Gil <luciano.gil@seidor.com>
2021-08-26 22:01:21 -0300

    Commit entrega

--- primera-clase/src/app/app-routing.module.ts
+++ primera-clase/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Ejercicio01Component } from './ejercicio01/ejercicio01.component';
 import { BienvenidaComponent } from './Ejercicio02/bienvenida/bienvenida.component';
 import { ErrorComponent } from './Ejercicio02/error/error.component';
 import { LoginComponent } from './Ejercicio02/login/login.component';
@@ -8,6 +9,7 @@ const routes: Routes =
 [
   {path:'login', component : LoginComponent},
   {path:'bienvenida',component:BienvenidaComponent},
+  {path: 'Ejercicio01', component: Ejercicio01Component},
   {path:'**',component:ErrorComponent}
 ];
 
